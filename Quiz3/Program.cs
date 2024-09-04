a43b1066f3405b06c4f795b36d90780236f0f770
rsgoodfellow <rsgoodfellow@amarillocollege.com>
2024-09-04 12:59:52 -0500

    Add project files.

---
+++ Quiz3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            bool isPal = true;
+
+            while (input != "exit")
+            {
+                Console.Write("Enter a string: ");
+                input = Console.ReadLine();
+
+                
+
+                for (int i = 0;  i < input.Length; i++)
+                {
+                    if (input[i] != input[input.Length - 1])
+                    {
+                        isPal = false;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Result: " + input + " is a palindrome.");
+                        break;
+                    }
+                }
+
+                if (isPal == false)
+                {
+                    Console.WriteLine("Result: " + input + " is not a palindrome.");
+                    break;
+                }
+            }
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
